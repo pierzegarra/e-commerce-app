7d3a447e9e6457a59fb718371464e713cd932b22
david <pier.zegarra.reymundo@gmail.com>
2025-12-02 07:25:57 -0500

    billingAddress and shippingAddress.

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -99,14 +99,26 @@ export class CheckoutComponent implements OnInit {
         if((event.target as HTMLInputElement).checked) {
             this.checkoutFormGroup.controls["billingAddress"]
                 .setValue(this.checkoutFormGroup.controls["shippingAddress"].value);
+
+            // bug fix for states
+            this.billingAddressStates = this.shippingAddressStates;
         } else {
             this.checkoutFormGroup.controls["billingAddress"].reset();
+
+            // bug fix for states
+            this.billingAddressStates = [];
         }
     }
 
     onSubmit() {
         console.log("Handling the submit button");
         console.log(this.checkoutFormGroup.get('customer')!!.value);
+        console.log("The email address is " + this.checkoutFormGroup.get('customer')?.value?.email);
+
+        console.log("The shipping address country is " + this.checkoutFormGroup
+            .get('shippingAddress')?.value?.country.name);
+        console.log("The shipping address state is " + this.checkoutFormGroup
+            .get('shippingAddress')?.value?.state.name);
     }
 
     handleMonthsAndYears() {
@@ -139,8 +151,8 @@ export class CheckoutComponent implements OnInit {
         const countryCode = formGroup?.value.country.code;
         const countryName = formGroup?.value.country.name;
 
-        console.log(`{formGroupCode} country code: ${countryCode}`);
-        console.log(`{formGroupName} country name: ${countryName}`);
+        console.log(`${formGroupName} country code: ${countryCode}`);
+        console.log(`${formGroupName} country name: ${countryName}`);
 
         this.luv2ShopFormService.getStates(countryCode).subscribe({
             next: (data: any) => {
